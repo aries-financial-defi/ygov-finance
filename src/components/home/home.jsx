a7fbbb11f0d2d03cc7617b1d6eea4a3a08c140dc
waltor <longmengua@gmail.com>
2020-10-17 15:44:50 +0800

    insert i18n feature

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,154 +1,156 @@
+//i18n => {"home.home.Text.1":"Stake","home.home.Text.2":"Vote","home.home.Text.3":"Lock"}
+import i18next from "i18next";
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import { withStyles } from '@material-ui/core/styles';
 import {
-  Card,
-  Typography,
+	Card,
+	Typography,
 } from '@material-ui/core';
 import { withNamespaces } from 'react-i18next';
-import { colors } from '../../theme'
+import { colors } from '../../theme';
 import HowToVoteIcon from '@material-ui/icons/HowToVote';
 import DetailsIcon from '@material-ui/icons/Details';
 import LockIcon from '@material-ui/icons/Lock';
 
 const styles = theme => ({
-  root: {
-    flex: 1,
-    display: 'flex',
-    width: '100%',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    flexDirection: 'column',
-    [theme.breakpoints.up('sm')]: {
-      flexDirection: 'row',
-    }
-  },
-  card: {
-    flex: '1',
-    height: '25vh',
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'column',
-    cursor: 'pointer',
-    borderRadius: '0px',
-    transition: 'background-color 0.2s linear',
-    [theme.breakpoints.up('sm')]: {
-      height: '100vh',
-      minWidth: '20%',
-      minHeight: '50vh',
-    }
-  },
-  stake: {
-    backgroundColor: colors.white,
-    '&:hover': {
-      backgroundColor: colors.pink,
-      '& .title': {
-        color: colors.white
-      },
-      '& .icon': {
-        color: colors.white
-      }
-    },
-    '& .title': {
-      color: colors.pink
-    },
-    '& .icon': {
-      color: colors.pink
-    }
-  },
-  vote: {
-    backgroundColor: colors.white,
-    '&:hover': {
-      backgroundColor: colors.blue,
-      '& .title': {
-        color: colors.white,
-      },
-      '& .icon': {
-        color: colors.white
-      }
-    },
-    '& .title': {
-      color: colors.blue,
-      display: 'block'
-    },
-    '& .soon': {
-      color: colors.blue,
-      display: 'none'
-    },
-    '& .icon': {
-      color: colors.blue
-    },
-  },
-  lock: {
-    backgroundColor: colors.white,
-    '&:hover': {
-      backgroundColor: colors.tomato,
-      '& .title': {
-        color: colors.white,
-      },
-      '& .icon': {
-        color: colors.white
-      }
-    },
-    '& .title': {
-      color: colors.tomato,
-    },
-    '& .icon': {
-      color: colors.tomato
-    },
-  },
-  title: {
-    padding: '24px',
-    paddingBottom: '0px',
-    [theme.breakpoints.up('sm')]: {
-      paddingBottom: '24px'
-    }
-  },
-  icon: {
-    fontSize: '60px',
-    [theme.breakpoints.up('sm')]: {
-      fontSize: '100px',
-    }
-  },
-  link: {
-    textDecoration: 'none'
-  }
+	root: {
+		flex: 1,
+		display: 'flex',
+		width: '100%',
+		justifyContent: 'space-around',
+		alignItems: 'center',
+		flexDirection: 'column',
+		[theme.breakpoints.up('sm')]: {
+			flexDirection: 'row',
+		}
+	},
+	card: {
+		flex: '1',
+		height: '25vh',
+		width: '100%',
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		flexDirection: 'column',
+		cursor: 'pointer',
+		borderRadius: '0px',
+		transition: 'background-color 0.2s linear',
+		[theme.breakpoints.up('sm')]: {
+			height: '100vh',
+			minWidth: '20%',
+			minHeight: '50vh',
+		}
+	},
+	stake: {
+		backgroundColor: colors.white,
+		'&:hover': {
+			backgroundColor: colors.pink,
+			'& .title': {
+				color: colors.white
+			},
+			'& .icon': {
+				color: colors.white
+			}
+		},
+		'& .title': {
+			color: colors.pink
+		},
+		'& .icon': {
+			color: colors.pink
+		}
+	},
+	vote: {
+		backgroundColor: colors.white,
+		'&:hover': {
+			backgroundColor: colors.blue,
+			'& .title': {
+				color: colors.white,
+			},
+			'& .icon': {
+				color: colors.white
+			}
+		},
+		'& .title': {
+			color: colors.blue,
+			display: 'block'
+		},
+		'& .soon': {
+			color: colors.blue,
+			display: 'none'
+		},
+		'& .icon': {
+			color: colors.blue
+		},
+	},
+	lock: {
+		backgroundColor: colors.white,
+		'&:hover': {
+			backgroundColor: colors.tomato,
+			'& .title': {
+				color: colors.white,
+			},
+			'& .icon': {
+				color: colors.white
+			}
+		},
+		'& .title': {
+			color: colors.tomato,
+		},
+		'& .icon': {
+			color: colors.tomato
+		},
+	},
+	title: {
+		padding: '24px',
+		paddingBottom: '0px',
+		[theme.breakpoints.up('sm')]: {
+			paddingBottom: '24px'
+		}
+	},
+	icon: {
+		fontSize: '60px',
+		[theme.breakpoints.up('sm')]: {
+			fontSize: '100px',
+		}
+	},
+	link: {
+		textDecoration: 'none'
+	}
 });
 
 class Home extends Component {
 
-  constructor(props) {
-    super()
+	constructor(props) {
+		super();
 
-    this.state = {
-    }
-  }
+		this.state = {
+		};
+	}
 
-  render() {
-    const { classes, t, location } = this.props;
+	render() {
+		const { classes, t, location } = this.props;
 
-    return (
-      <div className={ classes.root }>
-        <Card className={ `${classes.card} ${classes.stake}` } onClick={ () => { this.nav(location.pathname+'staking') } }>
-          <DetailsIcon className={ `${classes.icon} icon` } />
-          <Typography variant={'h3'} className={ `${classes.title} title` }>Stake</Typography>
-        </Card>
-        <Card className={ `${classes.card} ${classes.vote}` } onClick={ () => { this.nav(location.pathname+'vote') } }>
-          <HowToVoteIcon className={ `${classes.icon} icon` } />
-          <Typography variant={'h3'} className={ `${classes.title} title` }>Vote</Typography>
-        </Card>
-        {/*<Card className={ `${classes.card} ${classes.lock}` } onClick={ () => { this.nav(location.pathname+'lock') }}>
+		return (
+			<div className={ classes.root }>
+				<Card className={ `${classes.card} ${classes.stake}` } onClick={ () => { this.nav(location.pathname+'staking'); } }>
+					<DetailsIcon className={ `${classes.icon} icon` } />
+					<Typography variant={'h3'} className={ `${classes.title} title` } id="home.home.Text.1" >{i18next.t('home.home.Text.1')}</Typography>
+				</Card>
+				<Card className={ `${classes.card} ${classes.vote}` } onClick={ () => { this.nav(location.pathname+'vote'); } }>
+					<HowToVoteIcon className={ `${classes.icon} icon` } />
+					<Typography variant={'h3'} className={ `${classes.title} title` } id="home.home.Text.2" >{i18next.t('home.home.Text.2')}</Typography>
+				</Card>
+				{/*<Card className={ `${classes.card} ${classes.lock}` } onClick={ () => { this.nav(location.pathname+'lock') }}>
           <LockIcon className={ `${classes.icon} icon` } />
-          <Typography variant={'h3'} className={ `${classes.title} title` }>Lock</Typography>
+          <Typography variant={'h3'} className={ `${classes.title} title` } id="home.home.Text.3" >{i18next.t('home.home.Text.3')}</Typography>
         </Card>*/}
-      </div>
-    )
-  };
+			</div>
+		);
+	};
 
   nav = (screen) => {
-    this.props.history.push(screen)
+  	this.props.history.push(screen);
   }
 }
 
